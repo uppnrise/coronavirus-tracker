474be818b3f354bfbce8276857c2b13694246517
uppnrise <mtbnacar@gmail.com>
2020-05-27 02:47:25 +0300

    change from home to index

--- src/main/java/com/upp/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/upp/coronavirustracker/controllers/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalReportedCases);
         model.addAttribute("totalNewCases", totalNewCases);
-        return "home";
+        return "index";
     }
 
 }
